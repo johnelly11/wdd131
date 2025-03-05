e2c3fc873c53e386ecad72c11bf8e4cece327bf4
johnelly11 <jonathancnelson2003@gmail.com>
2025-03-04 21:03:25 -0700

    added more styling and js code

--- recipes/main.js
+++ recipes/main.js
@@ -2,29 +2,64 @@ import {recipes} from "./recipes.mjs"
 
 
 
+function starRating(rating) {
+    let stars = "";
+    const fullStars = Math.floor(rating);
+    const halfStarCheck = rating % 1 === 0.5;
+
+    let i = 0;
+    for (i; i < fullStars; i++) {
+        stars += "★";
+    }
+
+    if (halfStarCheck) {
+        stars += "⯪";
+    }
+
+    while (stars.length < 5) {
+        stars += "☆";
+    }
+
+    return stars;
+}
+
+
 function createArticles(recipes) {
     const container = document.getElementById("recipes-view");
     recipes.forEach(recipes => {
         const recipesDiv = document.createElement("div");
 
+        let tags = "";
+        recipes.tags.forEach((tag) => {
+            tags += `
+            <div class="recipe-tags">
+                <p>${tag}</p>
+            </div>
+            `;
+        });
+
+        
+
         const html = `
             <div class="recipe-view">
                 <div class="recipe-image">
-                    <img src="${recipes.image}">
+                    <img src="${recipes.image}"></img>
                 </div>
-                <div class="recipe-tags">
-                    <p>${recipes.tags}</p>
-                </div>
-                <div class="recipe-title">
-                    <h2>${recipes.name}</h2>
-                </div>
-                <div class="rating">
-                    <p>${recipes.rating}</p>
-                </div>
+                <div class="inner-recipe-view">
+                    <div class="recipe-tags-container">
+                        ${tags}
+                    </div>
+                    <div class="recipe-title">
+                        <h2>${recipes.name}</h2>
+                    </div>
+                    <div class="recipe-rating">
+                        <p>${starRating(recipes.rating)}</p>
+                    </div>
 
-                <div id="hidden">
-                    <div class="recipe-description">
-                        <p>${recipes.description}</p>
+                    <div id="hidden">
+                        <div class="recipe-description">
+                            <p>${recipes.description}</p>
+                        </div>
                     </div>
                 </div>
             </div>
